03e467c685e3e4e0e7909ac6c5d973842f848169
Simon-csy <ceres.1.sc@gmail.com>
2023-02-17 10:50:13 +0900

    change to await async

--- wiki.js
+++ wiki.js
@@ -9,34 +9,77 @@ const resultsContainer = document.getElementById('search-results');
 
     MIT License
 */
+var apiUrl = "https://en.wikipedia.org/w/api.php";
 
-var url = "https://en.wikipedia.org/w/api.php";
+const getPageId = async(search) => {
+    const params = {
+        action: "query",
+        list: "search",
+        srsearch: search,
+        format: "json"
+    };
 
-var params = {
-    action: "query",
-    list: "search",
-    srsearch: "Nelson Mandela",
-    format: "json"
+    apiUrl = apiUrl + "?origin=*";
+    Object.keys(params).forEach(function(key) { apiUrl += "&" + key + "=" + params[key]; });
+    console.log(apiUrl);
+    try {
+        const response = await fetch(apiUrl)
+            .then((value) => {
+                return value.json();
+            })
+            .catch((error) => {
+                alert("fetched error: " + JSON.stringify(error));
+            });
+        const pageid = response.query.search[0].pageid;
+        console.log('pageid ', pageid);
+        return pageid;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
+const getPageUrl = async(serch) => {
+    const pageid = await getPageId(serch);
 
-// Object.keys(params).forEach(function(key) { url += "&" + key + "=" + params[key]; });
+    const params = {
+        action: 'query',
+        prop: 'info',
+        pageids: pageid,
+        inprop: 'url',
+        format: 'json'
+    };
+    apiUrl = apiUrl + "?origin=*";
+    Object.keys(params).forEach(function(key) { apiUrl += "&" + key + "=" + params[key]; });
+    console.log(apiUrl);
+
+    try {
+        const response = await fetch(apiUrl)
+            .then((value) => {
+                return value.json();
+            })
+            .catch((error) => {
+                alert("fetched error: " + JSON.stringify(error));
+            });
+        console.log(response);
+        const page = response.query.pages[pageid];
+        const url = page.fullurl;
+        console.log('URL for page', pageid, ':', url);
+        return url;
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // prevent form from submitting normally
-    console.log('addEventListener');
-    const formData = new FormData(form);
-    params['srsearch'] = formData.get('q');
-    url = url + "?origin=*";
-    Object.keys(params).forEach(function(key) { url += "&" + key + "=" + params[key]; });
 
-    console.log(url);
-    fetch(url)
-        .then(function(response) { return response.json(); })
-        .then(function(response) {
-            console.log(response)
-            resultsContainer.innerHTML = JSON.stringify(response.query.search[0], null, 2);
+    const formData = new FormData(form);
+
+    getPageUrl(formData.get('q')).then(result => {
+            console.log(result);
         })
-        .catch(function(error) { console.log(error); });
+        .catch(error => {
+            console.error(error);
+        });
 });
